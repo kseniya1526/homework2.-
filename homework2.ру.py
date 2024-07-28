7061737f73479c9c2d0521eff0e55d0676a4df0e
kseniya1526 <kseniak505@gmail.com>
2024-07-28 20:33:23 +0700

    Add files via upload

---
+++ homework2.ру.py
@@ -0,0 +1,5 @@
+Number_of_homework = 12
+Number_of_hours = 1.5
+Course_name = 'Python'
+Time_for_one_homework = (Number_of_hours / Number_of_homework)
+print('Курс:',Course_name,',всего задач:',Number_of_homework,',затрачено часов:',Number_of_hours,',среднее время выполнения:',Time_for_one_homework,'часа')
